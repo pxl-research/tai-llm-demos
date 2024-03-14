ffe6f1ef95fb41d20ed3039ff9e7d98641e24d79
Servaas Tilkin <31237936+stilkin-pxl@users.noreply.github.com>
2024-03-14 11:21:32 +0100

    Added blocks example

---
+++ gradio-blocks/blocks.py
@@ -0,0 +1,26 @@
+import random
+import time
+
+import gradio as gr
+
+
+def respond(message, chat_history):
+    bot_message = random.choice(["How are you?", "I love you", "I'm very hungry"])
+    chat_history.append((message, bot_message))
+    time.sleep(0.5)
+    return "", chat_history
+
+
+with gr.Blocks() as blocks_ui:
+    # UI composition
+    chatbot = gr.Chatbot(label='Log')
+    with gr.Row():
+        msg = gr.Textbox(label='Prompt', scale=1)
+        send = gr.Button('Send', scale=0)
+    clear = gr.ClearButton([msg, chatbot])
+
+    # event handlers
+    msg.submit(respond, [msg, chatbot], [msg, chatbot])
+    send.click(respond, [msg, chatbot], [msg, chatbot])
+
+blocks_ui.launch()
