bef73ecd1831a12bb654f18505df4b0977f2b41a
Servaas Tilkin <31237936+stilkin-pxl@users.noreply.github.com>
2024-03-19 15:09:10 +0100

    FEAT: Added some icons to the UI

--- gradio-blocks/launch_ui.py
+++ gradio-blocks/launch_ui.py
@@ -36,9 +36,9 @@ with gr.Blocks(fill_height=True, title='PXL CheaPT') as llm_client_ui:
     cb_live = gr.Chatbot(label='Chat', scale=1)
     with gr.Group() as gr_live:
         with gr.Row():
-            tb_user = gr.Textbox(show_label=False, placeholder='Enter prompt here...', scale=10)
-            btn_send = gr.Button('Send', scale=0, min_width=64)
-            btn_clear = gr.Button('Clear', scale=0, min_width=64)
+            tb_user = gr.Textbox(lines=2, show_label=False, placeholder='Enter prompt here...', scale=10)
+            btn_send = gr.Button('', scale=0, min_width=64, icon='../assets/icons/send.png')
+            btn_clear = gr.Button('', scale=0, min_width=64, icon='../assets/icons/disposal.png')
     with gr.Row() as row_live:
         lbl_debug = gr.HTML()
 
@@ -60,8 +60,9 @@ with gr.Blocks(fill_height=True, title='PXL CheaPT') as llm_client_ui:
                 info="Select a log file to view the details",
                 scale=10
             )
-            btn_refresh = gr.Button('Reload', scale=0, min_width=64)
-            btn_clear = gr.ClearButton([cb_history], scale=0, min_width=64)
+            btn_refresh = gr.Button('', scale=0, min_width=64, icon='../assets/icons/refresh.png')
+            btn_clear = gr.ClearButton(value='', components=[cb_history], scale=0, min_width=64,
+                                       icon='../assets/icons/disposal.png')
 
     # event handlers
     btn_refresh.click(load_files, [dd_files], [dd_files])
@@ -69,11 +70,13 @@ with gr.Blocks(fill_height=True, title='PXL CheaPT') as llm_client_ui:
 
     # toggle UI
     with gr.Row():
-        btn_live = gr.Button('Chat')
-        btn_history = gr.Button('History')
+        btn_live = gr.Button('Chat', icon='../assets/icons/chat.png')
+        btn_history = gr.Button('History', icon='../assets/icons/history.png')
 
     # event handlers
     btn_live.click(show_live, [], [cb_live, gr_live, row_live, cb_history, gr_history])
     btn_history.click(show_history, [], [cb_live, gr_live, row_live, cb_history, gr_history])
 
 llm_client_ui.launch()
+
+# <a target="_blank" href="https://icons8.com/icon/90293/update-left-rotation">Refresh</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
