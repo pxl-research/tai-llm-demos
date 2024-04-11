21c40c5839cb6d3d2722e30a11a0eea40f5dedbc
Servaas Tilkin <31237936+stilkin-pxl@users.noreply.github.com>
2024-04-11 13:25:00 +0200

    FIX: Small code changes and clean-up

--- rag_demo/pymupdf_demo.py
+++ rag_demo/pymupdf_demo.py
@@ -3,33 +3,43 @@ from pprint import pprint
 import chromadb
 import fitz
 
-page_texts = []
-names = []
-count = 0
-reader = fitz.open("documents/ArbeidsreglementV8.pdf")
 
-# process the file
-for page in reader:
-    text = page.get_text()  # get page from pdf
-    page_texts.append(text)
-    count = count + 1
-    names.append(f"page_{count}")
-print(f"Converted {count} pages from pdf to text")
+def convert_pdf(filename):
+    page_texts = []
+    names = []
+    count = 0
+    reader = fitz.open(filename)
+
+    for page in reader:
+        text = page.get_text()  # convert page from pdf to text
+        page_texts.append(text)
+        count = count + 1
+        names.append(f"page_{count}")  # generate some page names
+        # TODO: add summary or other meta info
+    print(f"Converted {count} pages from '{filename}'")
+    return page_texts, names
+
+
+page_texts, names = convert_pdf("documents/ArbeidsreglementV8.pdf")
 
 # add the pages to the vector database
 cdb_client = chromadb.Client()
+# cdb_client = chromadb.PersistentClient(path="store/")
 collection = cdb_client.create_collection("arbeidsreglenemnt")
 
 collection.add(
     documents=page_texts,
     ids=names
 )
-print(f"Added {count} pages to chroma db")
+print(f"Added {len(names)} pages to chroma db")
 
 # perform a search on the vector database
+query = "Hoeveel uur per dag mag ik werken?"
+print(f"Query: '{query}'")
 results = collection.query(
-    query_texts=["Hoeveel uur per dag mag ik werken?"],
+    query_texts=[query],
     n_results=3,
 )
 
-pprint(results)
+pprint(results['distances'])
+pprint(results['ids'])
