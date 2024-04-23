e21c7deb7760db044aaf9df63396e69ffdf57d75
Servaas Tilkin <31237936+stilkin-pxl@users.noreply.github.com>
2024-04-23 12:55:38 +0200

    FEAT: can now add to chromadb using gradio interface

---
+++ rag_demo/blocks_chromadb_demo.py
@@ -0,0 +1,56 @@
+import json
+import os
+
+import chromadb
+import gradio as gr
+
+from fn_chromadb import add_pdf_to_db
+
+# cdb_client = chromadb.Client()  # in memory
+cdb_client = chromadb.PersistentClient(path="store/")  # on disk
+
+
+def pretty_print(json_obj):
+    print(json.dumps(json_obj, indent=2))
+
+
+# https://docs.trychroma.com/usage-guide#creating-inspecting-and-deleting-collections
+def cleanup_filename(full_file_path):
+    cleaned_name = os.path.basename(full_file_path)  # remove path
+    cleaned_name = os.path.splitext(cleaned_name)[0]  # remove extension
+    cleaned_name = cleaned_name.lower()  # lowercase
+    cleaned_name = cleaned_name.replace(".", "_")  # no periods
+    return cleaned_name[:60]  # crop it
+
+
+def on_file_uploaded(file_path, progress=gr.Progress()):
+    collection_name = cleanup_filename(file_path)
+    print(collection_name)
+    add_pdf_to_db(cdb_client, cleanup_filename(file_path), file_path, progress)
+
+    names = list_collections()
+    return [None, names]
+
+
+def list_collections():
+    collections_list = cdb_client.list_collections()
+    names = []
+    for collection in collections_list:
+        names.append([collection.name])
+
+    return names
+
+
+with gr.Blocks() as cdb_demo:
+    file_upload = gr.File(label="Click to Upload a File",
+                          file_types=[".pdf"],
+                          file_count="single")
+    df_files = gr.Dataframe(label="Collections",
+                            headers=['Name'],
+                            col_count=1,
+                            interactive=False)
+
+    file_upload.upload(on_file_uploaded, [file_upload], [file_upload, df_files])
+    cdb_demo.load(list_collections, [], [df_files])
+
+cdb_demo.queue().launch()
