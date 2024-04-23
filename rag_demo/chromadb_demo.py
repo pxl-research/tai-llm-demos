b33e578cb3424e5de0c5b2c3d552b4be8cffca72
Servaas Tilkin <31237936+stilkin-pxl@users.noreply.github.com>
2024-04-23 11:20:13 +0200

    UPDATE: changed chunk ids to be chunk numbers

--- rag_demo/chromadb_demo.py
+++ rag_demo/chromadb_demo.py
@@ -2,26 +2,26 @@ import json
 
 import chromadb
 
-from fn_chromadb import query_all_documents
+from fn_chromadb import query_all_documents, add_pdf_to_db
 
 
 def pretty_print(json_obj):
     print(json.dumps(json_obj, indent=2))
 
 
-# cdb_client = chromadb.Client()  # in memory
-cdb_client = chromadb.PersistentClient(path="store/")  # on disk
+cdb_client = chromadb.Client()  # in memory
+# cdb_client = chromadb.PersistentClient(path="store/")  # on disk
 
-# add_pdf_to_db(cdb_client, "focusproject", "documents/focusproject.pdf")
+add_pdf_to_db(cdb_client, "focusproject", "documents/focusproject.pdf")
 # add_pdf_to_db(cdb_client, "dienstverlening", "documents/dienstverlening.pdf")
 # add_pdf_to_db(cdb_client, "speerpuntprojecten", "documents/speerpuntprojecten.pdf")
 # add_pdf_to_db(cdb_client, "onderwijsinnovatiefonds", "documents/onderwijsinnovatiefonds.pdf")
 # add_pdf_to_db(cdb_client, "arbeidsregelement", "documents/arbeidsreglement.pdf")
 
 # perform a search on the vector database
-# query = "Wat is een focusproject precies?"
+query = "Wat is een focusproject precies?"
 # query = "Wat is de prijs van 1kg aardappelen?"
-query = "Hoeveel uur mag ik werken per dag?"
+# query = "Hoeveel uur mag ik werken per dag?"
 
 print(query)
 results = query_all_documents(cdb_client, query)
--- rag_demo/pdf_utils.py
+++ rag_demo/pdf_utils.py
@@ -18,21 +18,25 @@ def pages_to_chunks(page_list, document_name):
     chunk_meta_list = []
     chunk_id_list = []
     page_nr = 0
+    total_chunk_nr = 0
 
     for page_text in page_list:  # estimate about 4ms per page
         page_nr = page_nr + 1  # count pages
 
         chunks = re.split(r'\.\s', page_text)  # split phrases on period + whitespace
-        chunk_nr = 0
+        page_chunk_nr = 0
         for chunk in chunks:
-            if len(chunk) > 5:  # no tiny chunks please
-                chunk_nr = chunk_nr + 1
+            if len(chunk) > 5:  # no tiny chunks please TODO append tiny chunks to following chunk
+                page_chunk_nr = page_chunk_nr + 1  # count chunks per page
+                total_chunk_nr = total_chunk_nr + 1  # count total chunks
+
                 chunk_list.append(chunk)
+                chunk_id_list.append(f"{total_chunk_nr}")
                 meta = {'doc': document_name,
                         'page': page_nr,
-                        'chunk': chunk_nr}
-                # TODO: add summary or other meta info to chunk?
+                        'chunk': page_chunk_nr,
+                        'pos': total_chunk_nr}
                 chunk_meta_list.append(meta)
-                chunk_id_list.append(f"p{page_nr}_c{chunk_nr}")
+                # TODO: add summary or other meta info to chunk?
 
     return chunk_list, chunk_id_list, chunk_meta_list
