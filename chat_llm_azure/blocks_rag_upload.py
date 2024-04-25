a51925062736e2edcedefebfe42bbecbfc761e24
Servaas Tilkin <31237936+stilkin-pxl@users.noreply.github.com>
2024-04-25 12:54:53 +0200

    FEAT: now has RAG file upload

---
+++ chat_llm_azure/blocks_rag_upload.py
@@ -0,0 +1,40 @@
+import os
+import sys
+
+import chromadb
+import gradio as gr
+from dotenv import load_dotenv
+
+sys.path.append('../')
+
+from rag_demo.fn_chromadb import add_pdf_to_db
+
+load_dotenv()
+
+cdb_client = chromadb.Client()  # in memory
+cdb_path = os.getenv("CHROMA_LOCATION")
+# cdb_client = chromadb.PersistentClient(path=cdb_path)  # on disk
+
+
+# https://docs.trychroma.com/usage-guide#creating-inspecting-and-deleting-collections
+def cleanup_filename(full_file_path):
+    cleaned_name = os.path.basename(full_file_path)  # remove path
+    cleaned_name = os.path.splitext(cleaned_name)[0]  # remove extension
+    cleaned_name = cleaned_name.lower()  # lowercase
+    cleaned_name = cleaned_name.replace(".", "_")  # no periods
+    return cleaned_name[:60]  # crop it
+
+
+def on_file_uploaded(file_path, progress=gr.Progress()):
+    collection_name = cleanup_filename(file_path)
+    add_pdf_to_db(cdb_client, collection_name, file_path, progress)
+    names = list_collections()
+    return [None, names]
+
+
+def list_collections():
+    collections_list = cdb_client.list_collections()
+    names = []
+    for collection in collections_list:
+        names.append([collection.name])
+    return names
--- chat_llm_azure/launch_ui.py
+++ chat_llm_azure/launch_ui.py
@@ -6,6 +6,10 @@ from blocks_llm_chat_with_rag import (
     clear_log,
     client
 )
+from blocks_rag_upload import (
+    on_file_uploaded,
+    list_collections
+)
 from blocks_view_history import (
     load_files,
     file_selected,
@@ -26,7 +30,12 @@ def show_live():
 
         cb_history: gr.Chatbot(visible=False),
         gr_history: gr.Group(visible=False),
-        btn_history: gr.Button(interactive=True)
+        btn_history: gr.Button(interactive=True),
+
+        lbl_explainer: gr.HTML(visible=False),
+        file_upload: gr.File(visible=False),
+        df_files: gr.Dataframe(visible=False),
+        btn_upload: gr.Button(interactive=True)
     }
 
 
@@ -39,7 +48,30 @@ def show_history():
 
         cb_history: gr.Chatbot(visible=True),
         gr_history: gr.Group(visible=True),
-        btn_history: gr.Button(interactive=False)
+        btn_history: gr.Button(interactive=False),
+
+        lbl_explainer: gr.HTML(visible=False),
+        file_upload: gr.File(visible=False),
+        df_files: gr.Dataframe(visible=False),
+        btn_upload: gr.Button(interactive=True)
+    }
+
+
+def show_upload():
+    return {
+        cb_live: gr.Chatbot(visible=False),
+        gr_live: gr.Group(visible=False),
+        row_live: gr.Row(visible=False),
+        btn_live: gr.Button(interactive=True),
+
+        cb_history: gr.Chatbot(visible=False),
+        gr_history: gr.Group(visible=False),
+        btn_history: gr.Button(interactive=True),
+
+        lbl_explainer: gr.HTML(visible=True),
+        file_upload: gr.File(visible=True),
+        df_files: gr.Dataframe(visible=True),
+        btn_upload: gr.Button(interactive=False)
     }
 
 
@@ -49,6 +81,12 @@ def on_login(request: gr.Request):
     print(f"Created a thread with id: {new_thread.id} for user {user_folder}")
     return [set_folder(user_folder), new_thread]
 
+
+explainer = (
+    'Upload company or business information here if you want the language model to be able to perform lookup into it. '
+    'Do not upload data files. Stick to PDF only. '
+    'Processing may take considerable time depending on the size of the document. '
+    'Do not interrupt the processing step. There is no undo.')
 
 css = """
 .danger {background: red;} 
@@ -68,7 +106,7 @@ with (gr.Blocks(fill_height=True, title='PXL CheaPT', css=css) as llm_client_ui)
             btn_send = gr.Button('', scale=0, min_width=64, icon='../assets/icons/send.png')
             btn_remove = gr.Button('', scale=0, min_width=64, icon='../assets/icons/disposal.png')
     with gr.Row() as row_live:
-        lbl_debug = gr.HTML()
+        lbl_debug = gr.HTML('')
 
         # event handlers
         tb_user.submit(append_user, [tb_user, cb_live], [cb_live]
@@ -99,17 +137,46 @@ with (gr.Blocks(fill_height=True, title='PXL CheaPT', css=css) as llm_client_ui)
         btn_remove.click(remove_file, [dd_files, log_folder], [dd_files])
         dd_files.input(file_selected, [dd_files], [cb_history])
 
+    # file uppload UI
+    lbl_explainer = gr.HTML(explainer, visible=False)
+    file_upload = gr.File(label="Click to Upload a File",
+                          file_types=[".pdf"],
+                          file_count="single",
+                          visible=False)
+    df_files = gr.Dataframe(label="Collections",
+                            headers=['Name'],
+                            col_count=1,
+                            interactive=False,
+                            visible=False,
+                            scale=1)
+
+    file_upload.upload(on_file_uploaded, [file_upload], [file_upload, df_files])
+
     # toggle UI
     with gr.Row():
         btn_live = gr.Button('Chat', icon='../assets/icons/chat.png', interactive=False)
         btn_history = gr.Button('History', icon='../assets/icons/history.png')
+        btn_upload = gr.Button('Upload')
 
         # event handlers
-        btn_live.click(show_live, [], [cb_live, gr_live, row_live, cb_history, gr_history, btn_live, btn_history])
-        btn_history.click(show_history, [], [cb_live, gr_live, row_live, cb_history, gr_history, btn_live, btn_history])
-
+        btn_live.click(show_live, [],
+                       [cb_live, gr_live, row_live,
+                        cb_history, gr_history,
+                        file_upload, df_files, lbl_explainer,
+                        btn_live, btn_history, btn_upload])
+        btn_history.click(show_history, [],
+                          [cb_live, gr_live, row_live,
+                           cb_history, gr_history,
+                           file_upload, df_files, lbl_explainer,
+                           btn_live, btn_history, btn_upload])
+        btn_upload.click(show_upload, [],
+                         [cb_live, gr_live, row_live,
+                          cb_history, gr_history,
+                          file_upload, df_files, lbl_explainer,
+                          btn_live, btn_history, btn_upload])
     # global event
     llm_client_ui.load(on_login, None, [log_folder, thread])
+    llm_client_ui.load(list_collections, [], [df_files])
 
 # To create a public link, set `share=True` in `launch()`.
-llm_client_ui.launch(auth=auth_method, server_name='0.0.0.0')
+llm_client_ui.queue().launch(auth=auth_method, server_name='0.0.0.0')
