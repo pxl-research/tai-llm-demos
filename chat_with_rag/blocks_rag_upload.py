8ed64f0151a665a8225f2d6782f48f169095dfd9
Servaas Tilkin <31237936+stilkin-pxl@users.noreply.github.com>
2024-09-18 13:01:02 +0200

    FIX: removed some duplicate code

--- chat_with_rag/blocks_rag_upload.py
+++ chat_with_rag/blocks_rag_upload.py
@@ -1,5 +1,4 @@
 import os
-import re
 import sys
 
 import gradio as gr
@@ -14,21 +13,6 @@ load_dotenv()
 
 cdb_path = os.getenv("CHROMA_LOCATION")
 cdb_store = ChromaDocumentStore(path=cdb_path)  # on disk
-
-
-# https://docs.trychroma.com/usage-guide#creating-inspecting-and-deleting-collections
-def sanitize_filename(full_file_path):
-    cleaner_name = os.path.basename(full_file_path)  # remove path
-    cleaner_name = os.path.splitext(cleaner_name)[0]  # remove extension
-    cleaner_name = sanitize_string(cleaner_name)
-    return cleaner_name[:60]  # crop it
-
-
-def sanitize_string(some_text):
-    cleaner_name = some_text.strip()
-    cleaner_name = cleaner_name.replace(" ", "_")  # spaces to underscores
-    cleaner_name = re.sub(r'[^a-zA-Z0-9_-]', '-', cleaner_name)  # replace invalid characters with spaces
-    return cleaner_name
 
 
 def on_file_uploaded(file_list, progress=gr.Progress(track_tqdm=True)):
