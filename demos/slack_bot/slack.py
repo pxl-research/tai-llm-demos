b028e0c553d4fc9c7f8c9b52d82e97a2326db61e
Servaas Tilkin <31237936+stilkin-pxl@users.noreply.github.com>
2024-09-19 09:12:40 +0200

    CHANGE: updated slack bot demo script

--- demos/slack_bot/slack.py
+++ demos/slack_bot/slack.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from dotenv import load_dotenv
@@ -8,40 +9,40 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 load_dotenv()
 
 client = AzureOpenAI(
-    api_key=os.getenv("AOA_API_KEY"),
-    api_version="2024-02-15-preview",
-    azure_endpoint=os.getenv("AOA_ENDPOINT"),
+    api_key=os.getenv('AOA_API_KEY'),
+    api_version='2024-02-15-preview',
+    azure_endpoint=os.getenv('AOA_ENDPOINT'),
 )
 
 system_instruction = {
-    "role": "system",
-    "content": "I would like you to take a deep breath before responding."
-               "Always think step by step."
-               "Try to be concise, but precise as well."
+    'role': 'system',
+    'content': 'I would like you to take a deep breath before responding.'
+               'Always think step by step.'
+               'Try to be concise, but precise as well.'
 }
 
-# initializes slack app
-app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+# initializes Slack app
+app = App(token=os.environ.get('SLACK_BOT_TOKEN'))
 
 
-# Listens to incoming slash commands "/llm"
-@app.command("/llm")
+# Listens to incoming slash commands '/llm'
+@app.command('/llm')
 def handle_command(ack, say, command):
-    # Acknowledge the command request
-    ack()
-    history_openai_format = [system_instruction]
-    history_openai_format.append({"role": "user", "content": command['text']})
+    print(json.dumps(command, indent=1))
+    ack()  # acknowledge the command request
 
-    response = client.chat.completions.create(model='gpt-4-turbo-04-09',
+    history_openai_format = [
+        system_instruction,
+        {'role': 'user', 'content': command['text']}
+    ]
+    response = client.chat.completions.create(model='gpt-4o-mini',
                                               messages=history_openai_format,
                                               temperature=1.0)
     response_text = response.choices[0].message.content
-    print(response_text)
 
-    # Respond to the command
-    say(f"{response_text}")
+    say(f'{response_text}')  # respond to the command
 
 
-if __name__ == "__main__":
-    handler = SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN"))
+if __name__ == '__main__':
+    handler = SocketModeHandler(app, os.environ.get('SLACK_APP_TOKEN'))
     handler.start()
