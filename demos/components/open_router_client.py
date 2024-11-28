0e1025fc6e9f8100627a767646ddb98a13814316
Servaas Tilkin <31237936+stilkin-pxl@users.noreply.github.com>
2024-11-28 16:08:15 +0100

    FIX: removed some models without tool calling >_<

--- demos/components/open_router_client.py
+++ demos/components/open_router_client.py
@@ -41,9 +41,7 @@ class OpenRouterClient(OpenAI):
 
 
 # some models with tool calling (sorted from more to less powerful)
-GPT_4O_LATEST = 'openai/chatgpt-4o-latest'
 GEMINI_PRO_15 = 'google/gemini-pro-1.5',
-GPT_O1_MINI = 'openai/o1-mini'
 GPT_4O_1305 = 'openai/gpt-4o-2024-05-13'
 CLAUDE_35_SONNET_2006 = 'anthropic/claude-3.5-sonnet-20240620'
 LLAMA_405B_I = 'meta-llama/llama-3.1-405b-instruct',
@@ -56,8 +54,6 @@ QWEN_25_72B_I = 'qwen/qwen-2.5-72b-instruct'
 MISTRAL_LARGE_07 = 'mistralai/mistral-large-2407'
 LLAMA_31_70B_I = 'meta-llama/llama-3.1-70b-instruct',
 JAMBA_LARGE = 'ai21/jamba-1-5-large'
-GEMMA_2_72B = 'google/gemma-2-27b-it'
-YI_LARGE = '01-ai/yi-large'
 LLAMA_3_70B_I = 'meta-llama/llama-3-70b-instruct'
 CLAUDE_3_SONNET = 'anthropic/claude-3-sonnet'
 CLAUDE3_HAIKU = 'anthropic/claude-3-haiku',
