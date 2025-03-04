078597b283d6fda6642bb81e01dc74b0ce1215d4
Servaas Tilkin <31237936+stilkin-pxl@users.noreply.github.com>
2025-03-04 11:47:13 +0100

    FEAT: enabled flagging in basic demo

--- demos/basic_chat/chat_oai.py
+++ demos/basic_chat/chat_oai.py
@@ -65,5 +65,7 @@ def chat_completion(message, history):
 
 
 # https://www.gradio.app/guides/creating-a-chatbot-fast
-(gr.ChatInterface(chat_completion, type='messages')
+(gr.ChatInterface(chat_completion,
+                  type='messages',
+                  flagging_mode='manual')
  .launch(server_name='0.0.0.0', server_port=7020))
