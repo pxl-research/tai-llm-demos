b9fcb3c28b834e8cc4a7cd8e83dcb6eea0021148
Servaas Tilkin <31237936+stilkin-pxl@users.noreply.github.com>
2025-11-05 16:04:50 +0100

    CHANGE: updated to openai 2.x (model choice demo)

--- demos/model_choice/chat_with_model_choice.py
+++ demos/model_choice/chat_with_model_choice.py
@@ -171,7 +171,7 @@ custom_css = """
 with (gr.Blocks(fill_height=True, title='OpenRouter Model Choice', css=custom_css) as llm_client_ui):
     # state
     messages = gr.State([system_instruction])
-    selected_model = gr.State('z-ai/glm-4.6')
+    selected_model = gr.State('anthropic/claude-haiku-4.5')
     df_models = gr.State(None)
 
     # ui
